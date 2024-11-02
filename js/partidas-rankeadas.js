cb56b1dfa8e852d649e28601f94f417010487d29
Noeme Silva <noeme16@gmail.com>
2024-11-01 21:17:31 -0300

    Cria Calculadora de partidas Rankeadas

---
+++ js/partidas-rankeadas.js
@@ -0,0 +1,38 @@
+calcularRank(100, 10);
+
+function calcularRank(vitorias, derrotas) {
+  let nivel;
+  let saldoVitorias = vitorias - derrotas;
+
+  if (saldoVitorias < 10) 
+  {
+    nivel = "Ferro";
+  } 
+  else if (saldoVitorias >= 11 && saldoVitorias <= 20) 
+  {
+    nivel = "Bronze";
+  } 
+  else if (saldoVitorias >= 21 && saldoVitorias <= 50) 
+  {
+    nivel = "Prata";
+  } 
+  else if (saldoVitorias >= 51 && saldoVitorias <= 80) 
+  {
+    nivel = "Ouro";
+  } 
+  else if (saldoVitorias >= 81 && saldoVitorias <= 90) 
+  {
+    nivel = "Diamante";
+  }
+   else if (saldoVitorias >= 91 && saldoVitorias <= 100) 
+  {
+    nivel = "Lendário";
+  } 
+  else
+  {
+    nivel = "Imortal";
+  } 
+  console.log(
+    `O Herói tem de saldo de **${saldoVitorias}** está no nível de **${nivel}**`
+  )
+}
